cafa8883fb53121c48367da5982714646eeff570
Q. Price <priceqp@gmail.com>
2019-07-21 23:47:53 -0500

    Remove unnecessary components, rename RecordsDisplay component

--- src/RecordsDisplay.js
+++ src/RecordsDisplay.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import SquatRecords from './SquatRecords';
-import PushupRecords from './PushupRecords';
-import DipRecords from './DipRecords';
-import PlankRecords from './PlankRecords';
 
-class RecordsDisplay extends React.Component {
+class RecordsContainer extends React.Component {
     render() {
         return (
             <div>
-                <SquatRecords />
-                <br />
-                <PushupRecords />
-                <br />
-                <DipRecords />
-                <br />
-                <PlankRecords />
             </div>
         )
     }
 }
 
-export default RecordsDisplay;
+export default RecordsContainer;
