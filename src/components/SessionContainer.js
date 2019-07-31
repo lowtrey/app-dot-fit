b5f03a56664546ccb2021dcf0032db8f72275486
Q. Price <priceqp@gmail.com>
2019-07-31 13:37:55 -0500

    Refactor SessionContainer component using useState hook

--- src/components/SessionContainer.js
+++ src/components/SessionContainer.js
@@ -1,32 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Session from './Session';
 import SessionDialog from './SessionDialog';
 import '../styles/SessionContainer.css';
 
-class SessionContainer extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { sessionArr: [] };
-		this.addSession = this.addSession.bind(this);
+function SessionContainer() {
+	const [sessionArr, setSessionArr] = useState([]);
+	const addSession = (newSession) => {
+		setSessionArr([...sessionArr, newSession]);
 	};
-	addSession(newSession) {
-		this.setState({
-			sessionArr: [...this.state.sessionArr, newSession]
-		});
-	};
-
-	render() {
-		const sessions = this.state.sessionArr.map(
-			(s, i) => <Session key={i} exercise={s.exercise} reps={s.reps} />
-		);
-		return (
-			<Paper elevation={3} className='SessionContainer'>
-				<SessionDialog addSession={this.addSession} />
-				<div className='Cards'>{sessions}</div>
-			</Paper>
-		);
-	}
+	const sessions = sessionArr.map(
+		(s, i) => <Session key={i} exercise={s.exercise} reps={s.reps} />
+	);
+	
+	return (
+		<Paper elevation={3} className='SessionContainer'>
+			<SessionDialog addSession={addSession} />
+			<div className='Cards'>{sessions}</div>
+		</Paper>
+	);
 }
 
 export default SessionContainer;
